a5b83f4f3441a93cef7c88471d16c530ee9aa99e
ngg17 <71960903+ngg17@users.noreply.github.com>
2020-12-19 11:25:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song = "";
-
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload() {
     song = loadSound("ZAYN & Sia - Dusk Till Dawn.mp3");
 }
@@ -9,6 +12,24 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
 }
 
 function draw() {
@@ -17,4 +38,6 @@ function draw() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
