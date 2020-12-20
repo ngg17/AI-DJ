e510983979f272e7ef0833681c5873fb43914456
ngg17 <71960903+ngg17@users.noreply.github.com>
2020-12-20 12:53:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
 function preload() {
     song = loadSound("ZAYN & Sia - Dusk Till Dawn.mp3");
 }
@@ -23,6 +24,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
@@ -34,10 +37,31 @@ function gotPoses(results) {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FFA500");
+    stroke("#FF0000");
+    if(scoreLeftWrist > 0.2) {
+    circle(leftWristX, leftWristY, 20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);
+}
 }
 
 function play() {
     song.play();
-    song.setVolume(1);
     song.rate(1);
 }
+// Existing code unchanged.
+window.onload = function() {
+    var context = new AudioContext();
+    // Setup all nodes
+  }
+  
+  // One-liner to resume playback when user interacted with the page.
+  document.querySelector('button').addEventListener('click', function() {
+    context.resume().then(() => {
+      console.log('Playback resumed successfully');
+    });
+  });
