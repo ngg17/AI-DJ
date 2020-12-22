c87179ccd1eb0703a8634c6a293ce7a70a72004b
ngg17 <71960903+ngg17@users.noreply.github.com>
2020-12-22 15:49:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 function preload() {
     song = loadSound("ZAYN & Sia - Dusk Till Dawn.mp3");
 }
@@ -24,8 +25,9 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
@@ -39,6 +41,29 @@ function draw() {
     image(video, 0, 0, 600, 500);
     fill("#FFA500");
     stroke("#FF0000");
+    if (scoreRightWrist > 0.2) {
+    circle(rightWristX, rightWristY, 20);
+    if (rightWristY > 0 && rightWristY <= 100) {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if (rightWristY > 100 && rightWristY <= 200) {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY > 200 && rightWristY <= 300) {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY > 300 && rightWristY <= 400) {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if(rightWristY > 400 && rightWristY <= 500) {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
+    }
     if(scoreLeftWrist > 0.2) {
     circle(leftWristX, leftWristY, 20);
     InNumberleftWristY = Number(leftWristY);
@@ -53,15 +78,3 @@ function play() {
     song.play();
     song.rate(1);
 }
-// Existing code unchanged.
-window.onload = function() {
-    var context = new AudioContext();
-    // Setup all nodes
-  }
-  
-  // One-liner to resume playback when user interacted with the page.
-  document.querySelector('button').addEventListener('click', function() {
-    context.resume().then(() => {
-      console.log('Playback resumed successfully');
-    });
-  });
